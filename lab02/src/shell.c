31aad42c3bb9cf762ef0d1ae91ac23a8576922ad
BirddWang <f74111102@gs.ncku.edu.tw>
2024-10-28 16:50:18 +0800

    complete: 2-3, 2-4

--- lab02/src/shell.c
+++ lab02/src/shell.c
@@ -9,6 +9,21 @@
 #include "../include/command.h"
 #include "../include/builtin.h"
 
+void test_cmd_node(struct cmd_node *p) {
+	fprintf(stderr, "===== cmd_node =====\n");
+	for(int i=0; p->args[i] != NULL; i++) {
+		fprintf(stderr, "args[%d]: %s ", i, p->args[i]);
+	}
+	fprintf(stderr, "\n");
+	fprintf(stderr, "length: %d\n", p->length);
+	fprintf(stderr, "in_file: %s\n", p->in_file);
+	fprintf(stderr, "out_file: %s\n", p->out_file);
+	fprintf(stderr, "in: %d\n", p->in);
+	fprintf(stderr, "out: %d\n", p->out);
+	fprintf(stderr, "====================\n");
+	return;
+}
+
 // ======================= requirement 2.3 =======================
 /**
  * @brief 
@@ -20,7 +35,9 @@
  * 
  */
 void redirection(struct cmd_node *p){
+	// <
 	if(p->in_file) {
+		p->in = 3;
 		int fd = open(p->in_file, O_RDONLY);
 		if(fd == -1) {
 			perror("open");
@@ -32,7 +49,9 @@ void redirection(struct cmd_node *p){
 		}
 		close(fd);
 	}
+	// >
 	if(p->out_file) {
+		p->out = 4;
 		int fd = open(p->out_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		if(fd == -1) {
 			perror("open");
@@ -44,6 +63,7 @@ void redirection(struct cmd_node *p){
 		}
 		close(fd);
 	}
+	return;
 }
 // ===============================================================
 
@@ -64,8 +84,9 @@ int spawn_proc(struct cmd_node *p)
 	int status;
 	pid = fork();
 	if(pid == 0) {
+		redirection(p);
 		if(execvp(p->args[0], p->args) == -1){
-			perror("execvp");
+			fprintf(stderr, "execvp error\n");
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -73,6 +94,7 @@ int spawn_proc(struct cmd_node *p)
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
+	waitpid(pid, &status, 0);
   	return 1;
 }
 // ===============================================================
@@ -89,10 +111,83 @@ int spawn_proc(struct cmd_node *p)
  */
 int fork_cmd_node(struct cmd *cmd)
 {
-	int pipefd[2];
 	int status;
+	struct cmd_node *curr = cmd->head;
+	int cmd_count = 0;
+	while(curr) {
+		cmd_count++;
+		curr = curr->next;
+	}
+
+	int pipes[cmd_count-1][2];
+	for(int i=0; i<cmd_count-1; i++) {
+		if(pipe(pipes[i]) == -1) {
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	curr = cmd->head;
 	pid_t pid;
-	
+	int cmd_index = 0;
+	while(curr) {
+		pid = fork();
+		if(pid == 0) {
+			//first cmd
+			if(cmd_index == 0) {
+				close(pipes[cmd_index][0]);
+				if(curr->next != NULL) {
+					if(dup2(pipes[cmd_index][1], 1) == -1) {
+						perror("dup2");
+						exit(EXIT_FAILURE);
+					}
+				}
+			}
+			//last cmd
+			else if(cmd_index == cmd_count-1) {
+				close(pipes[cmd_index-1][1]);
+				if(dup2(pipes[cmd_index-1][0], 0) == -1) {
+					perror("dup2");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else {
+				if(dup2(pipes[cmd_index-1][0], 0) == -1) {
+					perror("dup2");
+					exit(EXIT_FAILURE);
+				}
+				if(dup2(pipes[cmd_index][1], 1) == -1) {
+					perror("dup2");
+					exit(EXIT_FAILURE);
+				}
+			}
+			for(int i=0; i<cmd_count-1; i++) {
+				close(pipes[i][0]);
+				close(pipes[i][1]);
+			}
+			redirection(curr);
+			test_cmd_node(curr);
+			if(execvp(curr->args[0], curr->args) == -1) {
+				perror("execvp");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if(pid < 0) {
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+
+		cmd_index++;
+		curr = curr->next;
+	}
+
+	for(int i=0; i<cmd_count-1; i++) {
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+	}
+	for(int i=0; i<cmd_count; i++) {
+		wait(&status);
+	}
 	return 1;
 }
 // ===============================================================
@@ -116,7 +211,7 @@ void shell()
 			status = searchBuiltInCommand(temp);
 			if (status != -1){
 				int in = dup(STDIN_FILENO), out = dup(STDOUT_FILENO);
-				if( in == -1 | out == -1)
+				if( in == -1 || out == -1)
 					perror("dup");
 				redirection(temp);
 				status = execBuiltInCommand(status,temp);
